ab179f321c7db9fdebdc2bdd1a63d165087c6780
doubleClickOnline <gediminas.jurk@gmail.com>
2018-03-17 21:04:48 +0200

    Created tests from TexttestFixture

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,73 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Test
+	public void foo() {
+		Item[] items = new Item[] { new Item("foo", 0, 0) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals("foo", app.items[0].name);
+	}
 
+	@Test
+	public void updateAfterOneDayTest() {
+
+		Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20), //
+				new Item("Aged Brie", 2, 0), //
+				new Item("Elixir of the Mongoose", 5, 7), //
+				new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+				new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+				// this conjured item does not work properly yet
+				new Item("Conjured Mana Cake", 3, 6) };
+
+		GildedRose app = new GildedRose(items);
+
+		int days = 1;
+		for (int i = 0; i < days; i++) {
+			System.out.println("-------- day " + i + " --------");
+			System.out.println("name, sellIn, quality");
+			for (Item item : items) {
+				System.out.println(item);
+			}
+			System.out.println();
+			app.updateQuality();
+		}
+	}
+
+	@Test
+	public void updateAfterTwoDaysTest() {
+
+		Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20), //
+				new Item("Aged Brie", 2, 0), //
+				new Item("Elixir of the Mongoose", 5, 7), //
+				new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+				new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+				// this conjured item does not work properly yet
+				new Item("Conjured Mana Cake", 3, 6) };
+		
+		GildedRose app = new GildedRose(items);
+
+		int days = 2;
+		for (int i = 0; i < days; i++) {
+			System.out.println("-------- day " + i + " --------");
+			System.out.println("name, sellIn, quality");
+			for (Item item : items) {
+				System.out.println(item);
+			}
+			System.out.println();
+			app.updateQuality();
+		}
+	}
+	
 }
